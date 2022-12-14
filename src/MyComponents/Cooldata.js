cb6b6e772230f40b2c123e085ecee5aedca51488
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2022-12-14 18:57:14 +0530

    last commit

--- src/MyComponents/Cooldata.js
+++ src/MyComponents/Cooldata.js
@@ -1,10 +1,13 @@
 // import React from 'react'
 import './style.css'
 
-import React from 'react';
+// import React from 'react';
 import Papa from 'papaparse';
 import Chart from 'react-google-charts'
 
+import ReactApexChart from 'react-apexcharts';
+import ApexCharts from 'apexcharts';
+import React, { useState, useEffect } from 'react';
 
 // const LineData = fetch('./graph1.csv')
 const LineChartOptions = {
@@ -31,12 +34,79 @@ export default function Cooldata(props) {
   //     },
   //   });
 
- 
+  const data =[10, 41, 35, 51, 49, 62, 69, 91, 148];
+  const series = [{
+    name: "Desktops",
+    data: data.slice()
+  }];
+
+  const options = {
+    chart: {
+      id: 'realtime',
+      height: 350,
+      type: 'line',
+      animations: {
+        enabled: true,
+        easing: 'linear',
+        dynamicAnimation: {
+          speed: 1000
+        }
+      },
+      toolbar: {
+        show: false
+      },
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    title: {
+      text: 'Dynamic Updating Chart',
+      align: 'left'
+    },
+    markers: {
+      size: 0
+    },
+    xaxis: {
+      type: 'datetime',
+      range: 10,
+    },
+    yaxis: {
+      max: 100
+    },
+    legend: {
+      show: false
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+
+    useEffect(()=>{
+    window.setInterval(() => {
+      getNewSeries(28, {
+        min: 10,
+        max: 90
+      })
+
+      ApexCharts.exec('realtime', 'updateSeries', [{
+        data: data,
+      }])
+    }, 1000)
+  })
+
+  
+
   return (
     <>
       <div className='head'>COOL DATA</div>
       <div className="graphs">
-        <div className="graphsub">
+        {/* <div className="graphsub">
           <div className="container mt-1">
             <Chart
               width={'15vw'}
@@ -105,8 +175,44 @@ export default function Cooldata(props) {
               rootProps={{ 'data-testid': '2' }}
             />
           </div>
-        </div>
+        </div> */}
+        <ReactApexChart options={options} series={series} type="line" height={200} />
+
+    
       </div>
     </>
   )
 }
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { LineChart, Line, XAxis, YAxis } from 'recharts';
+
+// const LiveChart = () => {
+//   const [data, setData] = useState([]);
+
+//   // Use the useEffect hook to fetch the data from the JSON file
+//   // when the component mounts
+//   useEffect(() => {
+//     fetch('data.json')
+//       .then(response => response.json())
+//       .then(json => setData(json));
+//   }, []);
+
+//   // Update the chart data every second
+//   setInterval(() => {
+//     setData(data.map(d => ({ x: d.x + 1, y: d.y + 1 })));
+//   }, 1000);
+
+//   return (
+//     <LineChart width={400} height={400} data={data}>
+//       <Line type="monotone" dataKey="y" stroke="#8884d8" />
+//       <XAxis dataKey="x" />
+//       <YAxis />
+//     </LineChart>
+//   );
+// };
+
+// export default LiveChart;
